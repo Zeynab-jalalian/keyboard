fb5136d4a00901724bace4dbfc2854bf3ea8154e
zeynab jalalian <zjalalianweb@gmail.com>
2024-12-27 11:50:11 +0330

    second way

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 window.addEventListener('keydown',(k)=>{
  document.getElementById('res').innerHTML=' The key you entered '+" = "+k.key+"<br>"+' its ASCII code '+' = '+k.keyCode;
 })
+
+
+/* second way 
+window.addEventListener('keydown',ac);
+function ac(k){
+ document.getElementById('res').innerHTML=' The key you entered '+" = "+k.key+"<br>"+' its ASCII code '+' = '+k.keyCode;
+} */
